f1992577db8399e51366980e6dd69a4fede7a26b
Joulence <4erviken@gmail.com>
2023-05-16 13:56:38 +0300

    Using the Custom Http Hook

--- src/App.js
+++ src/App.js
@@ -2,38 +2,26 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-learning-course-app-7-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-      );
+  const transformTask = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({
+    url: 'https://react-learning-course-app-7-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+  },
+  transformTask
+  );
 
   useEffect(() => {
     fetchTasks();
